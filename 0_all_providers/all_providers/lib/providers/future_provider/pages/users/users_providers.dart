932a05d4fb2f023375c62bc0febe6b401ad50f54
Mose <km_413@naver.com>
2024-05-10 17:01:00 +0900

    All Providers - 1. FutureProvider -  Riverpod Annotation

--- 0_all_providers/all_providers/lib/providers/future_provider/pages/users/users_providers.dart
+++ 0_all_providers/all_providers/lib/providers/future_provider/pages/users/users_providers.dart
@@ -1,8 +1,12 @@
 import 'package:all_providers/providers/future_provider/model/user.dart';
 import 'package:all_providers/providers/future_provider/provider/dio_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final userListProvider = FutureProvider.autoDispose<List<User>>((ref) async {
+part 'users_providers.g.dart';
+
+@riverpod
+FutureOr<List<User>> userList(UserListRef ref) async {
   ref.onDispose(() {
     print('[userListProvider]:: disposed');
   });
@@ -10,13 +14,34 @@ final userListProvider = FutureProvider.autoDispose<List<User>>((ref) async {
   final List userList = response.data;
   final users = [for (final user in userList) User.fromJson(user)];
   return users;
-});
+}
 
-final userDetailProvider = FutureProvider.autoDispose.family<User, int>((ref, id) async {
+// argument만 주면 family로 만들어줌
+@riverpod
+FutureOr<User> userDetail(UserDetailRef ref,int id) async {
   ref.onDispose(() {
     print('[userDetailProvider($id)]:: disposed');
   });
   final response = await ref.watch(dioProvider).get('/users/$id');
   final user = User.fromJson(response.data);
   return user;
-});
+}
+
+// final userListProvider = FutureProvider.autoDispose<List<User>>((ref) async {
+//   ref.onDispose(() {
+//     print('[userListProvider]:: disposed');
+//   });
+//   final response = await ref.watch(dioProvider).get('/users');
+//   final List userList = response.data;
+//   final users = [for (final user in userList) User.fromJson(user)];
+//   return users;
+// });
+//
+// final userDetailProvider = FutureProvider.autoDispose.family<User, int>((ref, id) async {
+//   ref.onDispose(() {
+//     print('[userDetailProvider($id)]:: disposed');
+//   });
+//   final response = await ref.watch(dioProvider).get('/users/$id');
+//   final user = User.fromJson(response.data);
+//   return user;
+// });
