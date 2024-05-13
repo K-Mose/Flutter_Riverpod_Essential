4935a3ba9b65e7d14243236c687ea152150bf0d9
Mose <km_413@naver.com>
2024-05-13 17:27:30 +0900

    All Providers -  1. StreamProvider - StreamProvider with annotation

--- 0_all_providers/all_providers/lib/providers/strem_provider/pages/ticker/ticker_provider.dart
+++ 0_all_providers/all_providers/lib/providers/strem_provider/pages/ticker/ticker_provider.dart
@@ -1,6 +1,23 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:async';
 
-final tickerProvider = StreamProvider<int>((ref) {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'ticker_provider.g.dart';
+
+
+// final tickerProvider = StreamProvider<int>((ref) {
+//   ref.onDispose(() {
+//     print("[tickerProvider]:: disposed");
+//   });
+//   return Stream.periodic(const Duration(seconds: 1), (t) {
+//     print(t);
+//     return t + 1;
+//   }).take(60);
+// });
+
+@riverpod
+Stream<int> ticker(TickerRef ref) {
   ref.onDispose(() {
     print("[tickerProvider]:: disposed");
   });
@@ -8,4 +25,4 @@ final tickerProvider = StreamProvider<int>((ref) {
     print(t);
     return t + 1;
   }).take(60);
-});
+}
