017a41f8c2d0ec3439879988a6840cf871f62b54
Mose <km_413@naver.com>
2024-11-06 16:27:53 +0900

    All Providers -  1. NotifierProvider - basic / autoDispose / family / autoDispose + family

--- 0_all_providers/all_providers/lib/providers/notifier_provider/pages/counter/counter_page.dart
+++ 0_all_providers/all_providers/lib/providers/notifier_provider/pages/counter/counter_page.dart
@@ -1,3 +1,4 @@
+import 'package:all_providers/providers/notifier_provider/pages/counter/counter_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,12 +7,41 @@ class CounterPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final counter = ref.watch(counterProvider);
+    final autoDisposeCounter = ref.watch(autoDisposeCounterProvider);
+    final familyCounter = ref.watch(familyCounterProvider(10));
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter'),
       ),
-      body: const Center(
-        child: Text('Counter'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("$counter", style: Theme.of(context).textTheme.headlineLarge,),
+                const SizedBox(width: 20,),
+                Text("$autoDisposeCounter", style: Theme.of(context).textTheme.headlineLarge,),
+                const SizedBox(width: 20,),
+                Text("$familyCounter", style: Theme.of(context).textTheme.headlineLarge,),
+              ],
+            ),
+            const SizedBox(height: 50,),
+            OutlinedButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).increment();
+                ref.read(autoDisposeCounterProvider.notifier).increment();
+                ref.read(familyCounterProvider(10).notifier).increment();
+              },
+              child: Text(
+                'Increment',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- 0_all_providers/all_providers/lib/providers/notifier_provider/pages/counter/counter_provider.dart
+++ 0_all_providers/all_providers/lib/providers/notifier_provider/pages/counter/counter_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// 일만적인 Notifier
 class Counter extends Notifier<int> {
   // 상태의 초기값
   @override
@@ -16,3 +17,61 @@ class Counter extends Notifier<int> {
 }
 
 final counterProvider = NotifierProvider<Counter, int> (Counter.new);
+
+// AutoDispose
+// autoDispose modifier를 적용하기 위해서는 Notifier가 아닌 AutoDisposeNotifier를 extends 해야 함
+class AutoDisposeCounter extends AutoDisposeNotifier<int> {
+
+  @override
+  int build() {
+    ref.onDispose(() {
+      print("[autoDisposeCounterProvider]:: disposed");
+    });
+    return 0;
+  }
+
+  void increment() {
+    state++;
+  }
+}
+
+final autoDisposeCounterProvider = NotifierProvider.autoDispose<AutoDisposeCounter, int>(AutoDisposeCounter.new);
+
+// Family
+// family modifier를 적용하기 위해서는 Notifier가 아닌 FamilyNotifier를 extends 해야 함
+// FamilyNotifier<상태 타입, 입력 타입>
+class FamilyCounter extends FamilyNotifier<int, int> {
+  // family의 경우에는 builder에 초기값을 보내줘야 하고, parameter 이름은 arg로 하는 것이 좋음
+  @override
+  int build(int arg) {
+    ref.onDispose(() {
+      print("[familyCounterProvider]:: disposed");
+    });
+    return arg;
+  }
+
+  void increment() {
+    state++;
+  }
+}
+// family modifier에는 추가적으로 받는 파라미터 타입을 넣어줌
+final familyCounterProvider = NotifierProvider.family<FamilyCounter, int, int> (FamilyCounter.new);
+
+// AutoDispose + Family
+// extends만 바뀌고 이하는 family와 같음
+class AutoDisposeFamilyCounter extends AutoDisposeFamilyNotifier<int, int> {
+
+  @override
+  int build(int arg) {
+    ref.onDispose(() {
+      print("[familyCounterProvider]:: disposed");
+    });
+    return arg;
+  }
+
+  void increment() {
+    state++;
+  }
+}
+
+final autoDisposeFamilyCounterProvider = NotifierProvider.family<AutoDisposeFamilyCounter, int, int> (AutoDisposeFamilyCounter.new);
