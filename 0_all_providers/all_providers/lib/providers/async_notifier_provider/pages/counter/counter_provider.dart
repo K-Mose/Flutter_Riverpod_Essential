12f9f87720d792672e90e3048abd86bbf714238e
Mose <km_413@naver.com>
2024-11-22 16:02:23 +0900

    All Providers -  1. AsyncNotifierProvider - riverpod generator

--- 0_all_providers/all_providers/lib/providers/async_notifier_provider/pages/counter/counter_provider.dart
+++ 0_all_providers/all_providers/lib/providers/async_notifier_provider/pages/counter/counter_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 /*
 AsyncNotifier에서 modifier 적용
@@ -9,6 +10,7 @@ family를 사용하기 위해서는 FamilyAsyncNotifier를 extends 하고 추가
 그리고 build에도 해당 타입의 값을 넘겨줘야 함
 autoDispose.family를 둘 다 사용하기 위해서는 AutoDisposeFamilyAsyncNotifier를 사용해야 함
  */
+/*
 class Counter extends AutoDisposeFamilyAsyncNotifier<int, int> {
   // FutureOr<T> return T or Future<T>
   @override
@@ -21,7 +23,7 @@ class Counter extends AutoDisposeFamilyAsyncNotifier<int, int> {
     // AsyncValue의 값을 0으로 emit
     return arg;
   }
-/*  @override
+*//*  @override
   FutureOr<int> build() {
     ref.onDispose(() {
       print('[counterProvider] disposed');
@@ -32,7 +34,7 @@ class Counter extends AutoDisposeFamilyAsyncNotifier<int, int> {
     // Future를 return 하면 지연이 없어도
     // 초기 값을 동기적으로 할당하기 위해 AsyncLoading이 발생 후 AsyncData가 호출된다.
     return Future.value(0);
-  }*/
+  }*//*
 
   Future<void> waitSecond() async => await Future.delayed(const Duration(seconds: 1));
 
@@ -76,3 +78,43 @@ class Counter extends AutoDisposeFamilyAsyncNotifier<int, int> {
 }
 
 final counterProvider = AsyncNotifierProvider.autoDispose.family<Counter, int, int>(Counter.new);
+*/
+
+part 'counter_provider.g.dart';
+
+@riverpod
+class Counter extends _$Counter {
+  // build paramter에 named parameter를 넣어도 됨
+  @override
+  FutureOr<int> build(int initialValue) async {
+    await waitSecond();
+    ref.onDispose(() {
+      print('[counterProvider] disposed');
+    },);
+    return initialValue;
+  }
+
+  Future<void> waitSecond() async => await Future.delayed(const Duration(seconds: 1));
+
+  /// Business Logic
+  Future<void> increment() async {
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() async {
+      await waitSecond();
+      if (state.value! > 2) {
+        throw "fail to increment";
+      }
+      return state.value! + 1;
+    },);
+  }
+
+  Future<void> decrement() async {
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() async {
+      await waitSecond();
+      return state.value! - 1;
+    },);
+  }
+}
